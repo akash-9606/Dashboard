b383c9c3e95da2cd04f10e0a4d0853b7a45e36fc
akash-9606 <akashkolaki5@gmail.com>
2025-05-17 14:33:11 +0530

    Delete Dashboard/frontend directory

--- Dashboard/frontend/src/pages/LandingPage.js
+++
@@ -1,9 +0,0 @@
-// src/pages/Dashboard.js
-import React from "react";
-import DashboardLayout from "../components/DashboardLayout";
-
-function LandingPage() {
-  return <DashboardLayout></DashboardLayout>;
-}
-
-export default LandingPage;
